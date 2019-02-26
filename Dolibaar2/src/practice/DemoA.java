6911b8df2cc912a061defdbbc71555ee570f3439
Aman9319 <Pushpendrasingh0193@gmail.com>
2019-02-26 12:38:37 +0530

    commit alll

---
+++ Dolibaar2/src/practice/DemoA.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class DemoA {
+	@Test
+	public void testA1()
+	{
+		
+	Reporter.log("Run testA1", true);	
+	Assert.fail();
+	}
+
+}
